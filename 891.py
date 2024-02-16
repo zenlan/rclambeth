afbe3d5010433de41f19a69335a2da396e0711f2
zenlan <mszpak@zenlan.com>
2024-02-16 19:46:26 +0000

    Adds stats to html

--- 891.py
+++ 891.py
@@ -6,8 +6,8 @@ import json
 def fetch_text(url):
     r = requests.get(url)
     if r.status_code == 200:
-        foo = json.loads(r.content)
-        json.dump(foo, open('891.json', 'w'))
-        open('891.js', 'w').write("data=" + json.dumps(foo))
+        data = json.loads(r.content)
+        json.dump(data, open('891.json', 'w'), indent=4, ensure_ascii=False)
+        open('891.js', 'w').write("response=" + json.dumps(data, indent=4, ensure_ascii=False))
 
 fetch_text('https://restarters.net/api/v2/groups/891')
